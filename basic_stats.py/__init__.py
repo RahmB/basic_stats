5e924ddf738e917d1369d0d4821589b2f9446045
RahmB <96204583+RahmB@users.noreply.github.com>
2021-12-15 19:49:05 -0500

    Create __init__.py

---
+++ basic_stats.py/__init__.py
@@ -0,0 +1,16 @@
+"""
+Statistics Package for Python
+    Version 1.0 Dec 15, 2021
+    Author:  Benjamin Rahming,Graduate School of Ocean Engineering, URI.
+    Email: brahming@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .basic_stats import *
+
+
+__all__ = ['mean','sd','norm','is_norm','is_skew', 'graph','stats']
